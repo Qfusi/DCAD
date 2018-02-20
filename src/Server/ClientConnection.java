efa84c4cd2ff4d08bc5d7262fadd75fb2dd5d939
Mario Willing <d15marwi@hs.local>
2018-02-20 16:55:57 +0100

    server och client connection added

---
+++ src/Server/ClientConnection.java
@@ -0,0 +1,52 @@
+
+package Server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ClientConnection {
+
+	private final String m_name;
+	private final InetAddress m_address;
+	private final int m_port;
+	private boolean activeStatus=true;
+
+	public ClientConnection(String name, InetAddress address, int port) {
+		m_name = name;
+		m_address = address;
+		m_port = port;
+		activeStatus=true;
+	}
+
+	public void sendMessage(String message, DatagramSocket socket,int k) {
+
+			byte[] x= (message).getBytes();
+			DatagramPacket dp= new DatagramPacket(x, x.length, m_address, m_port);
+			try {
+				socket.send(dp);
+
+			} catch (IOException e) {
+				
+				System.err.println("Something wrong with sending first message.");
+			}
+
+
+	}
+
+	public boolean hasName(String testName) {
+
+		return testName.equals(m_name);
+	}
+	public String getName() {
+		return m_name;
+	}
+	public void setActiveStatus(boolean newStatus) {
+		activeStatus=newStatus;
+	}
+	public boolean getActiveStatus() {
+		return activeStatus;
+	}
+
+}
