ff5032bf5895b71867b8d530d809812c35d4b040
Qfusi <Jonathan.Isaksson93@gmail.com>
2018-03-03 15:44:32 +0100

    Server stuff

--- src/Server/ReplicaServer.java
+++ src/Server/ReplicaServer.java
@@ -13,24 +13,23 @@ import Message.Message;
 import Message.RemoveMessage;
 
 public class ReplicaServer {
-	/*private ArrayList<FrontEndConnection> m_connectedClients = new ArrayList<FrontEndConnection>();
-	private String clientMessage="";
-	private String [] clientMessageArray;
-	private String serverMessage="";
-	private String ClientList;
-	private String privateMessage="";
-	private long startTime;*/
-	
-	//---------------CURRENTLY IN USE
+	//-------------------General
 	private ArrayList<ClientConnection> m_connectedClients = new ArrayList<ClientConnection>();
 	private ArrayList<GObject> m_GObjects  = new ArrayList<GObject>();
-	private FrontEndConnection m_connection;
+	private int m_ID;
+	
+	//-------------------FE
+	private FrontEndConnection m_FEconnection;
+	private InetAddress m_feAddress = null;
+	private int m_fePort;
+	
+	//-------------------UDP
 	private DatagramSocket m_socket;
 	private static InetAddress m_address = null;
 	private static int m_port;
-	private InetAddress m_feAddress = null;
-	private int m_fePort;
-	//-------------------------------
+	
+	//-------------------TCP
+	private ServerConnection m_serverConnection;
 	
 	public static void main(String[] args){
 		if(args.length < 1) {
@@ -40,7 +39,7 @@ public class ReplicaServer {
 		try {
 			ReplicaServer instance = new ReplicaServer();
 			instance.connectToFrontEnd();
-			if (instance.m_connection.handShake(m_address, m_port));
+			if (instance.m_FEconnection.handShake(m_address, m_port));
 				instance.listenForFrontEndMessages();
 		} catch(NumberFormatException e) {
 			System.err.println("Error: port number must be an integer.");
@@ -62,9 +61,11 @@ public class ReplicaServer {
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
+				m_ID = i;
+				TCPsetup(m_ID);
 				break;
 			} catch (SocketException e) {
-				System.err.println("Could not connect to row: " + i);
+				System.err.println("Could not create socket on row: " + i);
 			}
 		}
 	}
@@ -74,12 +75,8 @@ public class ReplicaServer {
 	private void listenForFrontEndMessages() {
 		System.out.println("Waiting for client messages... ");
 		
-		//---------------------------TRYING TO DO THINGS HERE------------------------------
-		//				~Jonathan
-		
-		
 		while(true) {
-			Message message = m_connection.receiveMessage();
+			Message message = m_FEconnection.receiveMessage();
 			
 			if (message instanceof JoinMessage) {
 				if (addClient(message.getAddress(), message.getPort())) {
@@ -92,245 +89,84 @@ public class ReplicaServer {
 				}
 				 
 				
-				m_connection.sendMessage(message);
+				m_FEconnection.sendMessage(message);
 			} else if (message instanceof DrawMessage) {
 				m_GObjects.add((GObject) message.getObj());
 				for (ClientConnection cc : m_connectedClients) {
 					message.setAddress(cc.getAddress());
 					message.setPort(cc.getPort());
-					m_connection.sendMessage(message);
+					m_FEconnection.sendMessage(message);
 				}
 			} else if (message instanceof RemoveMessage) {
 				m_GObjects.remove(m_GObjects.size() - 1);
 				for (ClientConnection cc : m_connectedClients) {
 					message.setAddress(cc.getAddress());
 					message.setPort(cc.getPort());
-					m_connection.sendMessage(message);
+					m_FEconnection.sendMessage(message);
 				}
 			}
+		}
+	}
+	
+	private void TCPsetup(int id) {
+		InetAddress address = null;
+		int port = 0;
+		int port2 = 0;
+		int port3 = 0;
+		try {
+			address = readAddressFromFile(id, new FileReader("resources/ServerConfig"));
+			port = readPortFromFile(id, 2, new FileReader("resources/ServerConfig"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		
-		//----------------------------------------------------------------------------
-		/*	startTime=System.currentTimeMillis();
-		    do {
-
-			byte[] buf= new byte[1024];
-			DatagramPacket dp = new DatagramPacket(buf, buf.length);
-			try {// all parts important to enable the server to answer
-				m_socket.receive(dp);
-				clientMessage = new String(dp.getData(),0,dp.getLength());
-				clientAdress= dp.getAddress();
-				clientPort= dp.getPort();
-
-
-			} catch (IOException e) {
-				System.err.println("Server: didnt recive message or translate to string");
-				System.exit(-1);
+		switch (id) {
+		case 0:
+			m_serverConnection = new ServerConnection(address, port);
+			break;
+		case 1:
+			try {
+				port2 = readPortFromFile(0, 3, new FileReader("resources/ServerConfig"));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
-			clientMessageArray = clientMessage.split(" ");
-			if(clientMessageArray.length>=2) {// checking if user has send a message with anything in it. /should maybe be done on client side before sending.!!!!!!!!!!!!
-
-
-
-
-				//-------------------------------------------------COMMANDS? ----------------------------------------------------------------
-				//reading commands and acting on it
-
-
-
-				//HANDSHAKE! --------------------------------------------------------------------------
-				if(clientMessageArray[1].equals("/connect")) {
-
-					//---------------------------connection process--------------------------------------
-					if(addClient(clientMessageArray[0],clientAdress,clientPort)) {
-
-						serverMessage="connecting";
-						sendPrivateMessage(serverMessage, clientMessageArray[0]);
-						broadcast(clientMessageArray[0]+" joined the chatroom ");
-					}
-				}
+			m_serverConnection = new ServerConnection(address, port, address, port2);
+			break;
+		case 2:
+			try {
+				port2 = readPortFromFile(0, 3, new FileReader("resources/ServerConfig"));
+				port3 = readPortFromFile(1, 3, new FileReader("resources/ServerConfig"));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
-			
-
-			else {//connection established, ready for use
-				if(clientMessageArray[1].equals("/connect")) {//for clients trying to use already taken user name				?????
-
-					serverMessage="cant connect, name already in use, by you ";
-					byte[] x= (serverMessage).getBytes();
-					DatagramPacket dpSend= new DatagramPacket(x, x.length, clientAdress, clientPort);
-					try {
-						m_socket.send(dpSend);																// kan inte skicka med sendPrivate... för att den inte finns med i listan
-						//testing
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						System.err.println("Something worong with sending first message.");
-					}
-				}
-
-				//-------------------------------------------PRIVATE MESSAGE---------------------------------------------------			
-				else if (clientMessageArray[1].equals("/tell")) {
-					if(!checkIfConnected(clientMessageArray[2])){//if receivername is not registered in server
-						privateMessage="username not found, try /list commando";
-						sendPrivateMessage(privateMessage, clientMessageArray[0]);
-
-
-					}
-					else {
-
-						privateMessage="";
-						//putting togheter message from array elements.
-						for(int i=3; i<clientMessageArray.length;i++) {
-							privateMessage=privateMessage+" "+ clientMessageArray[i];
-						}
-						serverMessage= clientMessageArray[0]+ ": "+ privateMessage;//name of sender + message ->for receiver
-						sendPrivateMessage(serverMessage, clientMessageArray[2]); //to receiver
-						serverMessage= clientMessageArray[2]+ ": "+ privateMessage;//name of receiver + message -> for sender
-						sendPrivateMessage(serverMessage, clientMessageArray[0]); //to sender
-					}
-				}
-				//------------------------------------------PRINT LIST-----------------------------------------------------------			
-				else if (clientMessageArray[1].equals("/list")) {
-
-					updateUserList();
-					sendPrivateMessage(ClientList, clientMessageArray[0]);
-
-				}
-				//-----------------------------------------leaving server---------------------------------			
-				else if (clientMessageArray[1].equals("/leave")) {
-
-					removeUser(clientMessageArray[0]);
-				}
-				else if (clientMessageArray[1].equals("/answer")) {
-
-
-					FrontEndConnection c;
-					for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-
-						c = itr.next();
-						if(c.hasName(clientMessageArray[0])) {
-							c.setActiveStatus(true);
-						}
-					}
-				}
-				else {
-					String broadMessage="";
-					for(int i=1; i<clientMessageArray.length;i++) {
-						broadMessage=broadMessage+" "+ clientMessageArray[i];
-					}
-					//	serverMessage= clientMessageArray[0]+ ": "+ broadMessage;//name of sender + message ->for receiver
-					broadcast(clientMessageArray[0]+": "+ broadMessage);
-				}
-				//--------------------------------------------------------------------------------------
-
-			}
-
-
-			kickInactiveClients();//checks after 20 seconds if anyone crashed
-		} while (true);*/
+			m_serverConnection = new ServerConnection(address, port, address, port2, address, port3);
+			break;
+		}
 	}
-
-	public boolean addClient(String name, InetAddress address, int port) {
-		/*FrontEndConnection c;
-		for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-			c = itr.next();
-			if(c.hasName(name)) {
-				return false; // Already exists a client with this name
+	
+	//WORKS
+	private void connectToFrontEnd() {
+		try {
+			m_feAddress = readAddressFromFile(1, new FileReader("resources/FrontEndConfig"));
+			m_fePort = readPortFromFile(1, 1, new FileReader("resources/FrontEndConfig"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+			
+		m_FEconnection = new FrontEndConnection(m_feAddress, m_fePort, m_socket);
+	}
+	
+	//WORKS
+	private boolean addClient(InetAddress address, int port) {
+		for (ClientConnection c : m_connectedClients) {
+			if (c.getAddress() == address && c.getPort() == port) {
+				System.out.println("didn't add client");
+				return false;
 			}
 		}
-		m_connectedClients.add(new FrontEndConnection(name, address, port));*/
+		m_connectedClients.add(new ClientConnection(address, port));
+		System.out.println("added client");
 		return true;
-	}
-
-	public void sendPrivateMessage(String message, String name) {
-		/*FrontEndConnection c;
-		for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-			c = itr.next();
-
-			if(c.hasName(name)) {
-				c.sendMessage(message, m_socket,10);
-			}
-		}*/
-	}
-
-	public void broadcast(String message) {
-		/*for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-			itr.next().sendMessage(message, m_socket,10);
-		}*/
-	}
-	private void updateUserList() {
-		/*FrontEndConnection c;
-		ClientList="Users on server: ";
-		for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-
-			c = itr.next();
-			ClientList= ClientList+ c.getName()+ ", " ;
-
-		}*/
-	}
-	private void removeUser(String name) {
-		/*FrontEndConnection c;
-		for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-			c = itr.next();
-
-			if(c.hasName(name)) {
-				broadcast(clientMessageArray[0]+" left the chatroom ");
-				m_connectedClients.remove(c);
-
-
-				return;
-
-			}
-		}*/
-	}
-	//needed for checking if user is on list or not without putting him/her on list if not
-	private Boolean checkIfConnected(String name) {
-		/*FrontEndConnection c;
-		for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-
-			c = itr.next();
-			if(c.hasName(name)) {
-				return true; // client is connected
-
-			}
-		}*/
-		return false;// client not connected
-	}
-	private void kickInactiveClients() {
-
-		/*if(startTime+20000<System.currentTimeMillis()) {
-
-			if(m_connectedClients.size()==0) {//check if there is a client on server at all, even if not needed in this version
-				startTime=System.currentTimeMillis();//do nothing while no clients registered, reset timer
-			}
-			else{
-
-
-				FrontEndConnection c;
-				for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-
-					c = itr.next();
-
-
-					if(!c.getActiveStatus()) { //if false, not active, meaning crashed
-						broadcast(c.getName()+" was kicked because of crash");
-						m_connectedClients.remove(c);
-						break;
-					}
-				}
-
-
-				String checkingClientsMessage="veryComplicatedWordSoNoUserWritesThisInChatAndActivatesTheProtocol";
-				broadcast(checkingClientsMessage);
-				startTime=System.currentTimeMillis();
-				//set all current clients to false
-				FrontEndConnection c2;
-				for(Iterator<FrontEndConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-
-					c2 = itr.next();
-					c2.setActiveStatus(false);
-				}
-			}
-		}*/
 	}
 	
 	//WORKS
@@ -349,6 +185,8 @@ public class ReplicaServer {
 		m_s.close();
 		return i;
 	}
+	
+	//WORKS
 	private static InetAddress readAddressFromFile(int row, FileReader file) {
 		Scanner m_s = null;
 		ArrayList<String> list = new ArrayList<String>();
@@ -368,30 +206,6 @@ public class ReplicaServer {
 			e.printStackTrace();
 			return null;
 		}
-	}
-	
-	//WORKS
-	private void connectToFrontEnd() {
-		try {
-			m_feAddress = readAddressFromFile(1, new FileReader("resources/FrontEndConfig"));
-			m_fePort = readPortFromFile(1, 1, new FileReader("resources/FrontEndConfig"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
-			
-		m_connection = new FrontEndConnection(m_feAddress, m_fePort, m_socket);
-	}
-	
-	private boolean addClient(InetAddress address, int port) {
-		for (ClientConnection c : m_connectedClients) {
-			if (c.getAddress() == address && c.getPort() == port) {
-				System.out.println("didn't add client");
-				return false;
-			}
-		}
-		m_connectedClients.add(new ClientConnection(address, port));
-		System.out.println("added client");
-		return true;
 	}
 }
 
--- src/Server/ServerConnection.java
+++ src/Server/ServerConnection.java
@@ -1,14 +1,47 @@
 package Server;
 
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerConnection {
+	//---------------------socket1
+	private ServerSocket m_Ssocket;
 	private  InetAddress m_address;
 	private  int m_port;
 	
+	//---------------------socket2
+	private Socket m_Csocket;
+	private InetAddress m_address2;
+	private  int m_port2;
+	
+	//---------------------socket3
+	private Socket m_Csocket2;
+	private InetAddress m_address3;
+	private  int m_port3;
+	
+	//SERVER 1
 	public ServerConnection(InetAddress address, int port) {
 		m_address = address;
 		m_port = port;
+	}
+	
+	//SERVER 2
+	public ServerConnection(InetAddress address, int port, InetAddress address2, int port2) {
+		m_address = address;
+		m_port = port;
+		m_address2 = address2;
+		m_port2 = port2;
+	}
+	
+	//SERVER 3
+	public ServerConnection(InetAddress address, int port, InetAddress address2, int port2, InetAddress address3, int port3) {
+		m_address = address;
+		m_port = port;
+		m_address2 = address2;
+		m_port2 = port2;
+		m_address3 = address3;
+		m_port3 = port3;
 	}
 	
 	public InetAddress getAddress() {
